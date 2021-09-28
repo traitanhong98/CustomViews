12f0f6e53d475c315fdec9fd48110186d8b230ef
ECO0542-HoangNM <hoangnguyen1@tdt.asia>
2021-09-28 10:33:40 +0700

    Popup

---
+++ CustomViews/CustomPopup/PopupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PopupViewController.swift
+//  CustomViews
+//
+//  Created by ECO0542-HoangNM on 28/09/2021.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
